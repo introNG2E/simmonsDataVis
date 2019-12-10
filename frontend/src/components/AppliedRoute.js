cda41a71409a6b9aed45a6a8908ddc49b2c91bef
Ivan Zundel <ivan.zundel11@gmail.com>
2019-12-09 20:25:52 -0500

    Revert "Update AppliedRoute.js"
    This reverts commit be87c898af52a1d5bef5520875f48aea6266072f.

--- frontend/src/components/AppliedRoute.js
+++ frontend/src/components/AppliedRoute.js
@@ -5,4 +5,4 @@ export default function AppliedRoute({ component: C, appProps, ...rest }) {
   return (
     <Route {...rest} render={props => <C {...props} {...appProps} />} />
   );
-}//this component creates a Route where the child component that it renders contains the passed in prop
+}
